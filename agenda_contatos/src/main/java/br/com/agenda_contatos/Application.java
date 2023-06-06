abea14f34918308188b687612683b67a92925374
RafaRz76Dev <rafaelraizer76@gmail.com>
2023-06-06 16:40:22 -0300

    primeiro commit

---
+++ agenda_contatos/src/main/java/br/com/agenda_contatos/Application.java
@@ -0,0 +1,17 @@
+// RESPONSAVEL POR INTERCEPTAR OS DADOS DE UMA APLICAÇÃO
+
+package br.com.agenda_contatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * Spring Boot application starter class
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args); //É a linha principal que roda o projeto
+    }
+}
